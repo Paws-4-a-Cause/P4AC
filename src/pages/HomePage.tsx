7efa47c010e5f2076bfbd1bea9bca5dfd98f9553
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-01-01 21:53:13 +0800

    Add React Router

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1> Paws 4 a Cause </h1>
+      <h3>
+        {" "}
+        An RC4 student led initiative to empower meaningiful contributions to
+        animal shelters{" "}
+      </h3>
+
+      <h2> Organisations we are working with </h2>
+    </div>
+  );
+};
+
+export default HomePage;
