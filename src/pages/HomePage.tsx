84d733db07a54fa16f761658a0605ad640e51f6b
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-01-10 01:01:00 +0800

    chore: migrate css to chakra props for home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,57 @@
 import Partners from "@/components/Partners";
 import image from "../assets/p4ac.png";
 import Organisation from "../components/Organisation";
-import { Heading } from "@chakra-ui/react";
-
+import { Box, Heading, Flex, Image, Text } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
     <>
-      <div id="home">
-        <div id="homedescription" style={{ width: "400px" }}>
-          <Heading as="h1" size="2xl" pb="4">
-            Paws for a Cause
-          </Heading>
-          <Heading as="h3" size="md">
-            {" "}
-            An <span id="highlighttext">RC4 </span> student led initiative to
-            empower meaningiful contributions to animal shelters{" "}
-          </Heading>
-        </div>
+      <Box id="home" p={4} as="section">
+        <Flex
+          justify="center"
+          align="center"
+          direction={{ base: "column", md: "row" }}
+          gap={8}
+          w="100%"
+        >
+          <Box
+            id="homedescription"
+            w={{ base: "100%", md: "60%", lg: "40%" }}
+            textAlign="left"
+          >
+            <Heading
+              as="h1"
+              size={{ base: "xl", md: "2xl", lg: "3xl" }}
+              pb="4"
+            >
+              Paws for a Cause
+            </Heading>
+            <Text
+              as="h3"
+              fontSize={{ base: "sm", md: "md", lg: "lg" }}
+              lineHeight="1.6"
+            >
+              An{" "}
+              <Text
+                as="span"
+                fontWeight="600"
+                color="#009bf591"
+                display="inline"
+              >
+                RC4
+              </Text>{" "}
+              student-led initiative to empower meaningful contributions to
+              animal shelters.
+            </Text>
+          </Box>
 
-        <img src={image} style={{ width: "400px" }} />
-      </div>
+          <Image
+            src={image}
+            maxW={{ base: "100%", md: "400px" }}
+          />
+        </Flex>
+      </Box>
+
       <Organisation />
       <Partners />
     </>
